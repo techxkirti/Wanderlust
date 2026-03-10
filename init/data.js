45d0023c636b3d41d086bed49406d687a7660d96
techxkirti <techexkirti@gmail.com>
2026-03-10 13:02:49 +0530

    Added styling

--- init/data.js
+++ init/data.js
@@ -5,7 +5,7 @@ const sampleListings = [
       "Escape to this charming beachfront cottage for a relaxing getaway. Enjoy stunning ocean views and easy access to the beach.",
     image: {
       filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      url: "https://images.unsplash.com/photo-1570129477492-45c003edd2be?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aG91c2V8ZW58MHx8MHx8fDA%3D",
     },
     price: 1500,
     location: "Malibu",
--- models/listing.js
+++ models/listing.js
@@ -1,18 +1,26 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const defaultImage = "https://images.unsplash.com/photo-1772797583328-f83bc3f94f80?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
 const listingSchema = new Schema({
     title: {
-        type:String,
+        type: String,
         required: true
     },
     description: {
         type: String
     },
     image: {
-        type: Object,
-        default: "https://images.unsplash.com/photo-1772797583328-f83bc3f94f80?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set: (v) => v === "" ? "https://images.unsplash.com/photo-1772797583328-f83bc3f94f80?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+        url: {
+            type: String,
+            default: defaultImage,
+            set: (v) => v === "" ? defaultImage : v
+        },
+        filename: {
+            type: String,
+            default: "listingimage"
+        }
     },
     price: {
         type: Number
@@ -22,8 +30,9 @@ const listingSchema = new Schema({
     },
     country: {
         type: String
-    },
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
+
 module.exports = Listing;
