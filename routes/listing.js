f2784a72bbcfbf1b45cb89a1eb09c8d5af5b6332
techxkirti <techexkirti@gmail.com>
2026-04-02 10:39:39 +0530

    implement file upload functionality using Multer for local storage

--- routes/listing.js
+++ routes/listing.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing} = require("../middleware.js");
-
 const listingController = require("../controllers/listing.js");
+
+const multer = require("multer");
+const upload = multer({ dest: 'uploads/' });
 
 //New Route:
 router.get("/new", 
@@ -18,11 +20,14 @@ router
     .get( 
         wrapAsync( listingController.index)
     )
-    .post( 
-        isLoggedIn, 
-        validateListing, 
-        wrapAsync( listingController.createListing )
-);
+//     .post( 
+//         isLoggedIn, 
+//         validateListing, 
+//         wrapAsync( listingController.createListing )
+// );
+    .post( upload.single("listing[image]") , (req, res) => {
+        res.send(req.file);
+    });
 
 router
     .route("/:id")
